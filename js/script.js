5f8fcf1cefc297f9271e3933f69b5e4bffd283e7
LucasCastro@94 <lucascastro_94@hotmail.com>
2023-08-10 11:37:04 -0300

    transition

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ const btnMenu = document.getElementById('btn-click')
 const menuIcon = document.getElementById('menu-icon')
 const secoes = document.querySelectorAll('section');
 const navLinksMobile = menuDiv.querySelectorAll('a');
+const logoImg = document.getElementById('logoImg')
+const historia =  document.getElementById('historia')
 
+window.addEventListener('load', () => {
+    logoImg.classList.add('transition-img')
+    historia.classList.add('transition-img')
+});
 
 
 btnMenu.addEventListener('click', abrirMenu)
